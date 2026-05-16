f7efc1828dde531c9e100c10c204c4c82ac03487
zzx4930 <3049834264@qq.com>
2026-05-16 22:21:36 +0800

    完成实验2调试

--- 作业3/ex02/lab02/实验二/sketch_may16a/sketch_may16a.ino
+++ 作业3/ex02/lab02/实验二/sketch_may16a/sketch_may16a.ino
@@ -11,9 +11,9 @@ void setup() {
 void loop() {
   digitalWrite(ledPin, HIGH);  // 点亮LED
   Serial.println("LED ON");    // 串口输出提示
-  delay(1000);                 // 保持1秒（1000毫秒）
+  delay(3000);                 // 保持1秒（1000毫秒）
   
   digitalWrite(ledPin, LOW);   // 熄灭LED
   Serial.println("LED OFF");   // 串口输出提示
-  delay(1000);                 // 保持1秒
+  delay(3000);                 // 保持1秒
 }
